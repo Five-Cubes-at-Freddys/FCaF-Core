0ed1d2fae8800556b20b17b29991641c5e1d8918
tarturr <tartur.dev@gmail.com>
2023-07-06 20:48:32 +0200

    Empty plugin created

---
+++ src/main/java/fr/tartur/fcaf/FCaF_Core.java
@@ -0,0 +1,17 @@
+package fr.tartur.fcaf;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FCaF_Core extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
