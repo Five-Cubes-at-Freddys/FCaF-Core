68905df064740b51b777c1ff611e48d83dabf4b3
tarturr <tartur.dev@gmail.com>
2023-07-19 14:05:48 +0200

    Simplified classes and added some documentation

--- src/main/java/fr/tartur/fcaf/libs/plugincomponents/commands/BaseCommand.java
+++ src/main/java/fr/tartur/fcaf/libs/plugincomponents/commands/BaseCommand.java
@@ -2,9 +2,18 @@ package fr.tartur.fcaf.libs.plugincomponents.commands;
 
 import fr.tartur.fcaf.user.FPlayerManager;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 
+/**
+ * Class used to define more specific uses of a basic CommandExecutor.
+ * Can contain subcommands in itself.
+ *
+ * @author tarturr
+ * @see org.bukkit.command.CommandExecutor
+ */
 public abstract class BaseCommand implements CommandExecutor {
 
     protected final String[] names;
@@ -12,6 +21,15 @@ public abstract class BaseCommand implements CommandExecutor {
     protected final FPlayerManager players;
     protected final BaseCommand[] subCommands;
 
+    /**
+     * Default class constructor.
+     *
+     * @param names       All command names, separated by a comma, especially used for subcommands to manage every case.
+     * @param argIndex    The index where the command is supposed to be located, starting from the parent command.
+     *                    If value is -1 (or another negative number), it means that the current command is a parent command.
+     * @param players     The connected fcaf players list.
+     * @param subCommands The possible subcommands.
+     */
     public BaseCommand(String names, int argIndex, FPlayerManager players, BaseCommand... subCommands) {
         this.names = names.split(",");
         this.index = argIndex;
@@ -19,9 +37,33 @@ public abstract class BaseCommand implements CommandExecutor {
         this.subCommands = subCommands;
     }
 
+    /**
+     * Class constructor with argIndex set to -1.
+     * This constructor is used for parent commands.
+     * @see #BaseCommand(String, int, FPlayerManager, BaseCommand...) 
+     */
+    public BaseCommand(String names, FPlayerManager players, BaseCommand... subCommands) {
+        this(names, -1, players, subCommands);
+    }
+
+    /**
+     * Returns the range of args data between currentIndex + 1 and args.length.
+     * @param currentIndex The provided current index.
+     * @param args         The string array from which to take the tail.
+     * @return The new array.
+     */
     protected String[] tail(int currentIndex, String[] args) {
         return args.length - currentIndex > 1
                 ? Arrays.copyOfRange(args, currentIndex + 1, args.length)
                 : new String[]{};
     }
+
+    /**
+     * The command to be runned if no subcommands provided by the player have been set or found.
+     * @param sender The basic command sender.
+     * @param label  The command label.
+     * @param args   The args entered by the player.
+     * @return true if the command was correctly executed, false otherwise.
+     */
+    protected abstract boolean run(@NotNull CommandSender sender, @NotNull String label, @NotNull String[] args);
 }
--- src/main/java/fr/tartur/fcaf/libs/plugincomponents/commands/CommandRunner.java
+++ src/main/java/fr/tartur/fcaf/libs/plugincomponents/commands/CommandRunner.java
@@ -1,20 +1,41 @@
 package fr.tartur.fcaf.libs.plugincomponents.commands;
 
 import fr.tartur.fcaf.user.FPlayerManager;
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 
+/**
+ * Class used to define a simple command.
+ * @author tarturr
+ * @see fr.tartur.fcaf.libs.plugincomponents.commands.BaseCommand
+ * @see org.bukkit.command.CommandExecutor
+ */
 public abstract class CommandRunner extends BaseCommand {
 
+    /**
+     * Class constructor.
+     * @param names       All command names, separated by a comma, especially used for subcommands to manage every case.
+     * @param argIndex    The index where the command is supposed to be. If value is -1 (or another negative number), it means that the current command is a parent command.
+     * @param players     The connected fcaf players list.
+     * @param subCommands The possible subcommands.
+     */
     public CommandRunner(String names, int argIndex, FPlayerManager players, BaseCommand... subCommands) {
         super(names, argIndex, players, subCommands);
     }
 
+    /**
+     * CommandRunner implementation of onCommand() method.<br><br>
+     *
+     * It will look for a possible subcommand, and execute it if the index and name match these subcommand properties.
+     * @param sender  Source of the command
+     * @param command Command which was executed
+     * @param label   Alias of the command which was used, in lowercase
+     * @param args    Passed command arguments
+     * @return true if the command was correctly executed, otherwise false.
+     */
     @Override
     public final boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         for (int i = 0; i < args.length; i++) {
@@ -28,8 +49,6 @@ public abstract class CommandRunner extends BaseCommand {
             }
         }
 
-        return run(sender, args);
+        return run(sender, label, args);
     }
-
-    protected abstract boolean run(@NotNull CommandSender sender, @NotNull String[] args);
 }
